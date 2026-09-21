703999a63909300657833c7a4a09e95b35167226
Burak Karahan <burak.karahan@mail.ru>
2026-09-21 15:42:34 +0300

    build: pin ginsights formula to v0.1.0

--- Formula/ginsights.rb
+++ Formula/ginsights.rb
@@ -1,7 +1,8 @@
 class Ginsights < Formula
   desc "GitHub-style local repository insights as a single Go binary"
   homepage "https://github.com/MarlonJD/ginsights"
-  url "https://github.com/MarlonJD/ginsights.git", branch: "main"
+  url "https://github.com/MarlonJD/ginsights/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "2e3c3458d918ca5eedb3ab4024d27855ba95939581d453274cd065cc00c78b1b"
   head "https://github.com/MarlonJD/ginsights.git", branch: "main"
   license "GPL-3.0-or-later"
 
